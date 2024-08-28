5d56b174cd208a6cf16eb12df9dad2ed5ec4fccd
jennyjonesigwe <jennyjonesigwe@gmail.com>
2024-08-28 15:58:00 +0100

    Modified Day Of The Week To Include Month And Year

--- dayOfTheWeek.js
+++ dayOfTheWeek.js
@@ -2,8 +2,10 @@ const day = "Tuesday"
 
 const position = 2
 
-const outPut = "Today is " + day + "." + " It is the " + position + "nd day of the week" + "."
+const month = "August"
 
+const year =  2024
 
+const outPut = "Today is " + day + "." + " It is the " + position + "nd day of the week" + " in the month of " +  month + ", " + year + "."
 
 console.log(outPut)
